53ece847883340397c3ddda607744ef0065a3f83
Szabó Krisztián <h164161@stud.u-szeged.hu>
2024-03-24 15:34:17 +0100

    .gitignore configurálása

---
+++ CommunitySite/Components/Pages/Register.razor.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+
+namespace CommunitySite.Components.Pages
+{
+    public partial class Register
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        private MudForm form;
+    }
+}
