54fadf5ed42ecde6ee3d9a353ef44579bc8e7f7a
Teodor <fodor.teo@gmail.com>
2024-04-01 19:44:19 +0200

    add the custom record type

---
+++ CVBNMY/Word.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVBNMY
+{
+    internal record Word
+    {
+        private string _wholeWord;
+
+        private string _guessedCharacters;
+
+        public Word(string wholeWord, string guessedCharacters)
+        {
+            _wholeWord = wholeWord;
+            _guessedCharacters = guessedCharacters;
+        }
+
+        public string WholeWord
+        {
+            get { return _wholeWord; }
+        }
+
+        public void SetWholeWord(string wholeWord)
+        {
+            _wholeWord = wholeWord;
+        }
+
+        public string GuessedCharacters
+        {
+            get { return _guessedCharacters; }
+        }
+
+        public void SetGuessedCharacters(string guessedCharacters)
+        {
+            _guessedCharacters = guessedCharacters;
+        }
+    }
+}
