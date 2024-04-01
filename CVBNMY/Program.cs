5757968c7d39c57075409a4de06e3d32907619b6
Teodor <fodor.teo@gmail.com>
2024-04-01 18:50:58 +0200

    load words from a file

--- CVBNMY/Program.cs
+++ CVBNMY/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CVBNMY
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            WordLoader wl = new WordLoader();
+            string[] words = wl.ReadWords("C:\\Users\\Teodor\\Desktop\\egyetemikurzus-2024-tavasz\\CVBNMY\\WordFiles\\EasyWords.txt");
+        }
+    }
+}
---
+++ CVBNMY/WordLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVBNMY
+{
+    internal class WordLoader
+    {
+        public string[]? ReadWords(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("File path cannot be null or empty.");
+            }
+
+            string[] words;
+            try
+            {
+                words = File.ReadAllLines(filePath);
+
+                foreach (string word in words)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("Unable to access file");
+                words = null;
+            }
+            catch(IOException) 
+            {
+                Console.WriteLine("I/O exception occured while reading the file");
+                words = null;
+
+            }
+            return words;
+        }
+    }
+}
