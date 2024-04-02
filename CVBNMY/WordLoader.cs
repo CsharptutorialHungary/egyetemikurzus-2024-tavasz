478026f2df6ebf5806b057201bc008b10718ddfe
Teodor <fodor.teo@gmail.com>
2024-04-02 19:31:44 +0200

    add 'relative' file path handling

--- CVBNMY/WordLoader.cs
+++ CVBNMY/WordLoader.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace CVBNMY
 {
-    internal class WordLoader
+    internal static class WordLoader
     {
-        public string[]? ReadWords(string filePath)
+        private const string WORD_FILES_PATH =          @"\..\..\..\WordFiles\";
+        private const string EASY_WORD_FILE_PATH =      "EasyWords.txt";
+        private const string MEDIUM_WORD_FILE_PATH =    "MediumWords.txt";
+        private const string HARD_WORD_FILE_PATH =      "HardWords.txt";
+        public static string[]? ReadWords(string filePath)
         {
             if (string.IsNullOrEmpty(filePath))
             {
@@ -38,5 +42,36 @@ namespace CVBNMY
             }
             return words;
         }
+
+        public static string WordFilePath(int difficulty)
+        {
+            return CreateWordFilePath(difficulty);
+        }
+        private static string CreateWordFilePath(int difficulty)
+        {
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sFile = Path.Combine(sCurrentDirectory, @"..\..\..\WordFiles\");
+            switch (difficulty)
+            {
+                case 0:
+                {
+                    sFile = Path.Combine(sFile, EASY_WORD_FILE_PATH);
+                    break;
+                }
+                case 1:
+                {
+                        sFile = Path.Combine(sFile, MEDIUM_WORD_FILE_PATH);
+                        break;
+                }
+                case 2:
+                {
+                        sFile = Path.Combine(sFile, HARD_WORD_FILE_PATH);
+                        break;
+                }
+            }
+            return Path.GetFullPath(sFile);
+        }
     }
+
+  
 }
