8abff663d566ef2c9f9b7c255d92dbb496383c0c
Teodor <fodor.teo@gmail.com>
2024-04-04 12:46:56 +0200

    add ToString for validation

--- CVBNMY/HangmanGame.cs
+++ CVBNMY/HangmanGame.cs
@@ -56,6 +56,10 @@ namespace CVBNMY
             }
             return tempWords;
         }
-
+        public override string ToString()
+        {
+            string wordList = string.Join(", ", words.Select(word => word.WholeWord));
+            return $"Guesses remaining: {guesses}\nWords: {wordList}";
+        }
     }
 }
