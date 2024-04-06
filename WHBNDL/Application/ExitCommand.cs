245b0c04c4b88620c5c995fd08b87d3411027900
Lionel Vaskó <119498065+lionelvasko@users.noreply.github.com>
2024-04-06 19:50:32 +0200

    Az alkalmazáshoz szükséges szövegek létrehozása + ExitCommand

---
+++ WHBNDL/Application/ExitCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WHBNDL.Infrastructure;
+
+namespace WHBNDL.Application
+{
+    internal class ExitCommand : IShellCommand
+    {
+        public string Name => "exit";
+
+        public void Execute(IHost host, string[] args)
+        {
+            host.Exit();
+        }
+    }
+}
