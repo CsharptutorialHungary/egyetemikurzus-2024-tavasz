145efef939633a6c161bafba1b3da3e7d2ecdc45
Lionel Vaskó <119498065+lionelvasko@users.noreply.github.com>
2024-04-08 16:02:17 +0200

    Questions model

---
+++ WHBNDL/Domain/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHBNDL.Domain
+{
+    public sealed record class Question
+    {
+        public required string QuestionText { get; set; }
+        public required string GoodAnswerText { get; set; }
+        public required string[] BadAnswersArray { get; set; }
+    }
+}
