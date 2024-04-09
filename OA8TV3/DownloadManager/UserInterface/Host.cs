2f82d29ed1aa5384724d4eda067ae16c25e8edf8
Varga Zalán <h270513@stud.u-szeged.hu>
2024-04-09 18:07:54 +0200

    Input-output műveletek

---
+++ OA8TV3/DownloadManager/UserInterface/Host.cs
@@ -0,0 +1,26 @@
+using DownloadManager.Infrastructure;
+
+namespace DownloadManager.UserInterface;
+
+internal class Host : IHost
+{
+    public string ReadLine()
+    {
+        return Console.ReadLine() ?? throw new IOException("User input is empty");
+    }
+
+    public void WriteLine(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void Write(string message)
+    {
+        Console.Write(message);
+    }
+
+    public void Exit()
+    {
+        Environment.Exit(0);
+    }
+}
