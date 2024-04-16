b5c5f895f71ef0f7ab6ea18fa42938f01836c3e1
Teodor <fodor.teo@gmail.com>
2024-04-16 20:53:21 +0200

    add record class

---
+++ CVBNMY/PlayerScore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVBNMY
+{
+    internal sealed record PlayerScore
+    {
+        private string _word;
+
+        private int _score;
+
+        public PlayerScore(string word, int score)
+        {
+            Word = word;
+            Score = score;
+        }
+
+        public string Word { get;} 
+        public int Score { get; }
+
+    }
+
+}
