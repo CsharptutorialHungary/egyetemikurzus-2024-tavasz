e1c2171c33a3a96fc57d78e1eaa3b4504ba45033
Teodor <fodor.teo@gmail.com>
2024-04-17 16:03:16 +0200

    add init modifier

--- CVBNMY/PlayerScore.cs
+++ CVBNMY/PlayerScore.cs
@@ -8,19 +8,14 @@ namespace CVBNMY
 {
     internal sealed record PlayerScore
     {
-        private string _word;
-
-        private int _score;
+        public string Word { get; init; }
+        public int Score { get; init; }
 
         public PlayerScore(string word, int score)
         {
             Word = word;
             Score = score;
         }
-
-        public string Word { get;} 
-        public int Score { get; }
-
     }
 
 }
