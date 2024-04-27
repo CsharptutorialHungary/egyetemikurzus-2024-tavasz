2432248ecddacf60767d567dd5931841cd59d932
Molnar Peter Soma <h161736@stud.u-szeged.hu>
2024-04-27 16:42:57 +0200

    kerdes osztaly keszen van

---
+++ L5Y5D3/Kerdes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5Y5D3
+{
+    internal class Kerdes
+    {
+        public int A { get; }
+        public int B { get; }
+        public char Muvelet { get; }
+        public int Megoldas { get; }
+
+
+
+        public Kerdes(Random rand)
+        {
+            A = szamGeneralas(rand);
+            B = szamGeneralas(rand);
+
+            int veletlen = rand.Next(0, 4);
+
+            Muvelet = muveletGeneralas(veletlen);
+            Megoldas = valaszGeneralas(this.A, this.B, veletlen);
+
+        }
+
+        private int szamGeneralas(Random r)
+        {
+            return r.Next(-100,100);
+        }
+
+        private char muveletGeneralas(int muveletszam)
+        {
+            switch (muveletszam)
+            {
+                case 0: return '+';
+                case 1: return '-';
+                case 2: return '*';
+                case 3: return '/';
+                default: return 'H';
+
+            }
+        }
+
+        private int valaszGeneralas(int elso, int masodik, int muveletszam)
+        {
+            switch (muveletszam) {
+                case 0 : return elso + masodik;
+                case 1: return elso - masodik;
+                case 2: return elso * masodik;
+                case 3: return elso / masodik;
+                default: return 0;
+
+            }
+        }
+
+        
+    }
+}
--- L5Y5D3/Program.cs
+++ L5Y5D3/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using L5Y5D3;
+
 Console.WriteLine("Hello, World!");
+Random rand = new Random();
+
+Kerdes kerdes = new Kerdes(rand);
+Console.WriteLine(kerdes.ToString);
