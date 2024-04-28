8a0c323cef8d8657ef9211cb7c55288363d16b50
Döme Balázs Márk <h044345@stud.u-szeged.hu>
2024-04-28 20:14:09 +0200

    Initial commit

---
+++ Kvizapp/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+class Kviz {
+
+    static public void Main(String[] args)
+    {
+        
+    }
+
+    public record Player
+    {
+        public String name = "";
+        public int points = 0;
+    };
+
+    public record Question
+    {
+        
+    }
+}
