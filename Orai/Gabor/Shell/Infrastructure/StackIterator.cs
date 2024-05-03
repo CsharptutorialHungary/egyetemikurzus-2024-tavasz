98be655cbb529a8028238864d4db2bed5fd2c0ed
Ruzsinszki <gabor.ruzsinszki@evosoft.com>
2024-05-03 13:24:39 +0200

    Threads 2

---
+++ Orai/Gabor/Shell/Infrastructure/StackIterator.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections;
+
+namespace Shell.Infrastructure
+{
+    public class StackIterator : IEnumerator<int>
+    {
+        private readonly List<int> _list;
+        private int index = -1;
+
+        public int Current { get; set; }
+        object IEnumerator.Current { get; }
+
+        public StackIterator(List<int> list) 
+        {
+            _list = list;
+        }
+
+        public void Dispose()
+        {
+            //Intentionally empty
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            if (index >= _list.Count)
+            {
+                return false;
+            }
+            Current = _list[index];
+            return true;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+    }
+}
