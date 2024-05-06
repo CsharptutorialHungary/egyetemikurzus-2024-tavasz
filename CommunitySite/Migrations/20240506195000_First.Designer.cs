5c21c3bf52e637330b92f0909cd7043ba1f2d6d9
Szabó Krisztián <h164161@stud.u-szeged.hu>
2024-05-06 22:08:03 +0200

    5. mérföldkő

--- CommunitySite/Migrations/20240506195000_First.Designer.cs
+++ CommunitySite/Migrations/20240506195000_First.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CommunitySite.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Oracle.EntityFrameworkCore.Metadata;
 
@@ -11,9 +12,11 @@ using Oracle.EntityFrameworkCore.Metadata;
 namespace CommunitySite.Migrations
 {
     [DbContext(typeof(CommunitySiteContext))]
-    partial class CommunitySiteContextModelSnapshot : ModelSnapshot
+    [Migration("20240506195000_First")]
+    partial class First
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
