4574f471b9b6e777b6581caead199ef933f0ca5b
Norbert Bicok <norbertbicok12@gmail.com>
2024-05-08 13:15:57 +0200

    PlayerService létrehozása, hogy lehessen managelni több játékost.

---
+++ XWUH14/Application/PlayerService.cs
@@ -0,0 +1,28 @@
+﻿using XWUH14.Domain.Entities;
+
+namespace XWUH14.Application
+{
+    public class PlayerService
+    {
+        private readonly List<Player> _players;
+
+        public PlayerService(List<Player> players)
+        {
+            _players = players;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            _players.Add(player);
+        }
+        public Player TopPlayer()
+        {
+            return _players.OrderByDescending(player => player.Score).FirstOrDefault();
+        }
+
+        public IEnumerable<Player> GetPlayers()
+        {
+            return _players;
+        }
+    }
+}
