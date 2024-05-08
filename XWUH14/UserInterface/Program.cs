85edf0728f8e1990fb9b6113452c7026f6db414f
Norbert Bicok <norbertbicok12@gmail.com>
2024-05-08 16:47:44 +0200

    GameService interface játékmenet létrehozásához.

--- XWUH14/UserInterface/Program.cs
+++ XWUH14/UserInterface/Program.cs
@@ -1,28 +1,10 @@
-﻿using XWUH14.Application;
-using XWUH14.Domain.Entities;
-using XWUH14.Infrastructure;
-
-namespace XWUH14.UserInterface
+﻿namespace XWUH14.UserInterface
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            var players = new List<Player>
-            {
-                new Player("Player 1"),
-                new Player("Player 2")
-            };
-
-            var playerService = new PlayerService(players);
-            var questionProvider = new FileQuestionProvider("Assets/questions.json");
-            var answerValidator = new AnswerValidator();
-
-            var gameService = new GameService(questionProvider, answerValidator, playerService);
-
-            gameService.StartGameAsync().Wait();
-            Console.WriteLine("Vége a játéknak.");
-
+            Console.WriteLine("Hello World!");
         }
     }
 }
