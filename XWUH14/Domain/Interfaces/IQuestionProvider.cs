2aaee87db9a9fff408798c38c9d50caab3fac88f
Norbert Bicok <norbertbicok12@gmail.com>
2024-05-08 14:02:06 +0200

    Kérdés biztosító interface, 1 Task functionnel, hogy a json fájlbeolvasás async módon működjön.

---
+++ XWUH14/Domain/Interfaces/IQuestionProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using XWUH14.Domain.Entities;
+
+namespace XWUH14.Domain.Interfaces
+{
+    public interface IQuestionProvider
+    {
+        Task<IEnumerable<Question>> GetQuestionsAsync();
+    }
+}
