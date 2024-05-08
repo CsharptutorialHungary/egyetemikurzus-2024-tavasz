ab8835b5eebf474071c8730475a67ffa1c5f697c
Norbert Bicok <norbertbicok12@gmail.com>
2024-05-08 14:03:30 +0200

    Felesleges inputok levétele, fájlbeolvasás osztály létrehozása.

---
+++ XWUH14/Infrastructure/FileQuestionProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using XWUH14.Domain.Entities;
+using XWUH14.Domain.Interfaces;
+
+namespace XWUH14.Infrastructure
+{
+    public class FileQuestionProvider : IQuestionProvider
+    {
+        private readonly string _filePath;
+
+        public FileQuestionProvider(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public async Task<IEnumerable<Question>> GetQuestionsAsync()
+        {
+            throw new Exception("Not implemented");
+        }
+    }
+}
