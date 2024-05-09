ca83b9dbc13a48df836bbd4465b6ada01aeb358a
tolnaitamas <tolnaitamas6@gmail.com>
2024-05-09 16:31:27 +0200

    fix commnads and json files created

--- XWG8AW/Application/BestCommand.cs
+++ XWG8AW/Application/BestCommand.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using XWG8AW.Infrastructure;
+
 namespace XWG8AW.Application
 {
-    internal class BestCommand
+    internal class BestCommand : IShellCommand
     {
+        public string Name => "best";
+
+        public void Execute(IHost host, string[] args)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- XWG8AW/Application/ListCommand.cs
+++ XWG8AW/Application/ListCommand.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using XWG8AW.Infrastructure;
+
 namespace XWG8AW.Application
 {
-    internal class ListCommand
+    internal class ListCommand : IShellCommand
     {
+        public string Name => "list";
+
+        public void Execute(IHost host, string[] args)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
