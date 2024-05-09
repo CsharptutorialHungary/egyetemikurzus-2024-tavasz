788aff1dacbcb09d4dd8f173dafa216909f374fc
tolnaitamas <tolnaitamas6@gmail.com>
2024-05-09 20:10:05 +0200

    question deserialize type change

--- XWG8AW/Infrastructure/QuestionDeserializer.cs
+++ XWG8AW/Infrastructure/QuestionDeserializer.cs
@@ -12,7 +12,7 @@ namespace XWG8AW.Infrastructure
 {
     internal class QuestionDeserializer
     {
-        public async Task<Question[]> QuestionDeserializeFromJson()
+        public async Task<List<Question>> QuestionDeserializeFromJson()
         {
             string fullpath = Environment.CurrentDirectory;
             string path = fullpath.Substring(0, fullpath.Length - 16);
@@ -21,7 +21,7 @@ namespace XWG8AW.Infrastructure
             try {
                 using (var stream = File.OpenRead(correctPath))
                 {
-                    Question[]? questions = await JsonSerializer.DeserializeAsync<Question[]>(stream, new JsonSerializerOptions
+                    List<Question>? questions = await JsonSerializer.DeserializeAsync<List<Question>>(stream, new JsonSerializerOptions
                     {
                         AllowTrailingCommas = true,
                     });
