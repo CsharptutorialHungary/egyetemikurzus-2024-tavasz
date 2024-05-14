d549994ba51df6c5e5934ea11d0bdd9b8e98d45a
Práger Gergő <prager.gergo@gmail.com>
2024-05-14 22:13:25 +0200

    xml file pelda struktura dokumentacio

--- DDUFSL/XmlManager/XmlManager.cs
+++ DDUFSL/XmlManager/XmlManager.cs
@@ -9,6 +9,31 @@ using System.Xml.Linq;
 
 namespace DDUFSL.XmlManager
 {
+    /// <summary>
+    /// A 'config.xml' file-t az executable mellé kell rakni
+    /// 
+    /// Egy példa struktúra:
+    /// 
+    /// <FilterConfig>
+    ///     <Folder>C:\Users\GreG\Downloads\Qpak-server\mods</Folder>   //A mappa útvonala amit rendezni szeretnél
+    ///     <FileTypes> //FileTípusok
+    ///         <FileType> //Egy FileTípus object
+    ///             <Extension>.jar</Extension> //FileTípus kiterjesztése
+    ///             <Path>\java\jars</Path> //Megfelelő típusú file-ok mozgatási helye
+    ///         </FileType>
+    ///         <FileType>
+    ///             <Extension>.zip</Extension>
+    ///             <Path>\zips</Path>
+    ///         </FileType>
+    ///         <FileType>
+    ///             <Extension>.exe</Extension>
+    ///             <Path>\executables</Path>
+    ///         </FileType>
+    ///     </FileTypes>
+    /// </FilterConfig>
+    /// 
+    /// 
+    /// </summary>
     internal class XmlManager : IXmlManager
     {
         XmlDocument _document { get; set; }
