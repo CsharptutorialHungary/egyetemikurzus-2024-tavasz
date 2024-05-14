1a4f66311f6b09fe2a180c0cbf3f6885e661d930
Práger Gergő <prager.gergo@gmail.com>
2024-05-14 21:43:02 +0200

    XmlManager kiszervezesenek probalasa

---
+++ DDUFSL/XmlManager/IXmlManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DDUFSL.XmlManager
+{
+    internal interface IXmlManager
+    {
+        public List<string> getFileTypes();
+        public string getDirectoryPath();
+        public XmlNodeList getFileTypePaths();
+    }
+}
---
+++ DDUFSL/XmlManager/XmlManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace DDUFSL.XmlManager
+{
+    internal class XmlManager : IXmlManager
+    {
+        XmlDocument _document { get; set; }
+        public XmlManager() 
+        {
+            _document = new XmlDocument();
+            try
+            {
+                _document.Load("config.xml");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(0);
+                return;
+            }
+            catch (XmlException e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(0);
+                return;
+            }
+        }
+        public string getDirectoryPath()
+        {
+            return _document.DocumentElement.SelectSingleNode("Folder").InnerText ?? "";
+        }
+
+
+        public List<string> getFileTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public XmlNodeList getFileTypePaths()
+        {
+            return _document.DocumentElement?.SelectNodes("/FilterConfig/FileTypes/FileType");
+        }
+    }
+}
